5fed6ae33c31f02b7bf749222ed779ec45b41eab
pisix <leundeufranky@gmail.com>
2016-11-06 06:32:02 +0100

    Creation du service MessageService pour partager l'utilisation des Toasts et des alertes entres les différentes vues de l'application

---
+++ src/services/message.service.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by fleundeu on 05/11/2016.
+ */
+
+import {Injectable} from '@angular/core'
+import {Http} from "@angular/http";
+import {GlobalsConstants} from "../constants/globals.constants";
+import {Observable} from "rxjs/Observable";
+import { ToastController, LoadingController,AlertController} from 'ionic-angular';
+
+
+
+@Injectable()
+export class MessageService{
+
+  constructor(public alertCtrl:AlertController,public loadingCtrl:LoadingController,private toastCtrl: ToastController) {
+  }
+
+  showToast(message)
+  {
+    /*Toast.show(message, '5000', 'bottom').subscribe(
+     toast => {
+     console.log(toast);
+     }
+     );*/
+
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  showAlert(message,title)
+  {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
+}
